c171b8f6e0090466561296150cb81c230426804d
Cody Doucette <doucette@bu.edu>
2020-09-25 17:23:43 -0400

    Add legit TCP client to IP list

--- ec2/ec2_get_ip_addrs.sh
+++ ec2/ec2_get_ip_addrs.sh
@@ -2,7 +2,7 @@
 
 key_name="gatekeeper.pem"
 
-nodes=(client gk1_server gk2_server gt_server router nat_server dest)
+nodes=(client gk1_server gk2_server gt_server router nat_server dest tcp)
 fname="ip_addrs.txt"
 
 rm -f ${fname}
