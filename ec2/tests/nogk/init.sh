7c7e83a7e9474b90bc0c953187faed39f12efbfd
Cody Doucette <doucette@bu.edu>
2020-10-13 17:04:31 -0400

    Update no defense scripts

--- ec2/tests/nogk/init.sh
+++ ec2/tests/nogk/init.sh
@@ -7,5 +7,6 @@ if [ $# -ne 0 ]; then
 fi
 
 ./ec2_send_command.sh client "sudo ip link set down ens6"
-./ec2_send_command.sh client "sudo ip route del 172.31.3.200/32 encap bpf xmit obj /ebpf-encap/ipip.bpf sec ipip_encap dev ens5"
+./ec2_send_command.sh tcp "sudo ip link set dev ens5 mtu 1500"
+./ec2_send_command.sh tcp "sudo ip route del 172.31.3.200/32"
 ./ec2_send_command.sh tcp "git clone http://github.com/cjdoucette/raw-packets.git"
--- ec2/tests/nogk/run.sh
+++ ec2/tests/nogk/run.sh
@@ -1,22 +1,40 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]; then
-  echo "usage: ./run.sh traffic_rate experiment_length"
-  echo "e.g.: ./run.sh 100mbps 30"
+if [ $# -ne 1 ]; then
+  echo "usage: ./run.sh traffic_rate"
+  echo "e.g.: ./run.sh {100mbps,500mbps,1-9gbps}"
   exit
 fi
 
 EXP_RATE=$1
-EXP_LEN=$2
 EXP_NAME=nogk$(echo "${@}" | tr [:blank:] _)
 
+# Don't need to do these because the instance is restarted.
+#./ec2_send_command.sh dest "sudo pkill bash"		# ifconfig
+#./ec2_send_command.sh dest "sudo pkill nc"		# nc/tcp
+#./ec2_send_command.sh dest "sudo pkill python3"		# http
+#./ec2_send_command.sh dest "sudo pkill tcpdump"
+#./ec2_send_command.sh dest "rm -rf /home/ubuntu/server_ifconfig.txt"
+#./ec2_send_command.sh dest "rm -rf /home/ubuntu/server_dump.pcap"
+
+./ec2_send_command.sh client "sudo pkill bash"		# ifconfig
 ./ec2_send_command.sh client "sudo pkill sendRaw"
-./ec2_send_command.sh dest "sudo pkill nc"
 ./ec2_send_command.sh client "rm -rf /home/ubuntu/client_ifconfig.txt"
-./ec2_send_command.sh dest "rm -rf /home/ubuntu/server_ifconfig.txt"
 
-./ec2_send_command.sh dest "nohup bash -c 'nc -k -l 1234' &>/dev/null &"
+./ec2_send_command.sh tcp "sudo pkill nc"		# nc/tcp
+./ec2_send_command.sh tcp "sudo pkill curl"		# http
+./ec2_send_command.sh tcp "sudo pkill send.sh"
+./ec2_send_command.sh tcp "sudo pkill tcpdump"
+./ec2_send_command.sh tcp "rm -rf /home/ubuntu/raw-packets/legit/legit_log.txt"
 
+# Start netcat server.
+#./ec2_send_command.sh dest "nohup bash -c 'nc -k -l 1234' &>/dev/null &"
+# Start HTTP server.
+./ec2_send_command.sh dest "nohup bash -c 'python3 SimpleHTTPServerWithUpload.py' &>/dev/null &"
+# Start tcpdump (for debugging only).
+#./ec2_send_command.sh dest "nohup bash -c 'sudo tcpdump -i ens5 not port 22 -w /home/ubuntu/server_dump.pcap' &>/dev/null &"
+
+# Start attack traffic.
 if [ "$EXP_RATE" == "100mbps" ]; then
 	./ec2_send_command.sh client "nohup bash -c 'cd raw-packets; sudo make; sudo ./sendRaw 0 ${EXP_RATE}' &>/dev/null &"
 elif [ "$EXP_RATE" == "500mbps" ]; then
@@ -77,25 +95,44 @@ elif [ "$EXP_RATE" == "10gbps" ]; then
 	./ec2_send_command.sh client "nohup bash -c 'cd raw-packets; sudo make; sudo ./sendRaw 2 ${EXP_RATE}' &>/dev/null &"
 fi
 
-# Let senders get up to speed.
-sleep 30
+# Start capturing attacker TX rate and server RX rate.
+./ec2_send_command.sh client "nohup bash -c 'while true; do ifconfig | grep ens5 --after-context=8 >> /home/ubuntu/client_ifconfig.txt && sleep 1; done' &>/dev/null &"
+./ec2_send_command.sh dest "nohup bash -c 'while true; do ifconfig | grep ens5 --after-context=8 >> /home/ubuntu/server_ifconfig.txt && date >> /home/ubuntu/server_ifconfig.txt && sleep 5; done' &>/dev/null &"
 
-./ec2_send_command.sh client "nohup bash -c 'for i in $(eval echo {1..$((${EXP_LEN}+6))}); do ifconfig | grep ens5 --after-context=8 >> /home/ubuntu/client_ifconfig.txt && sleep 1; done' &>/dev/null &"
-./ec2_send_command.sh dest "nohup bash -c 'for i in $(eval echo {1..$((${EXP_LEN}/5))}); do ifconfig | grep ens5 --after-context=8 >> /home/ubuntu/server_ifconfig.txt && date >> /home/ubuntu/server_ifconfig.txt && sleep 5; done' &>/dev/null &"
+# Let attackers get up to speed.
+sleep 20
 
-./ec2_send_command.sh tcp "nohup bash -c 'cd raw-packets/legit && ./send.sh' &>/dev/null &"
-sleep ${EXP_LEN}
+# Start legitimate client.
+./ec2_send_command.sh tcp "nohup bash -c 'sudo tcpdump -i ens5 not port 22 -w /home/ubuntu/legit_dump.pcap' &>/dev/null &"
+./ec2_send_command.sh tcp "cd raw-packets/legit && ./send.sh"
 
+echo "Cleaning up ..."
+./ec2_reboot_instance.sh dest
+echo "Instance restarting ..."
+sleep 60
+#./ec2_send_command.sh dest "sudo pkill bash"		# ifconfig
+#./ec2_send_command.sh dest "sudo pkill nc"		# nc/tcp
+#./ec2_send_command.sh dest "sudo pkill python3"		# http
+#./ec2_send_command.sh dest "sudo pkill tcpdump"
+#./ec2_get_file.sh dest "/home/ubuntu/server_ifconfig.txt" results/${EXP_NAME}
+#./ec2_send_command.sh dest "rm -rf /home/ubuntu/server_ifconfig.txt"
+#./ec2_get_file.sh dest "/home/ubuntu/server_dump.pcap" results/${EXP_NAME}
+#./ec2_send_command.sh dest "rm -rf /home/ubuntu/server_dump.pcap"
+
+./ec2_send_command.sh client "sudo pkill bash"		# ifconfig
 ./ec2_send_command.sh client "sudo pkill sendRaw"
-./ec2_send_command.sh dest "sudo pkill nc"
-./ec2_send_command.sh tcp "sudo pkill nc"
+./ec2_get_file.sh client "/home/ubuntu/client_ifconfig.txt" results/${EXP_NAME}
+./ec2_send_command.sh client "rm -rf /home/ubuntu/client_ifconfig.txt"
+
+./ec2_send_command.sh tcp "sudo pkill nc"		# nc/tcp
+./ec2_send_command.sh tcp "sudo pkill curl"		# http
 ./ec2_send_command.sh tcp "sudo pkill send.sh"
 ./ec2_send_command.sh tcp "sudo pkill tcpdump"
+./ec2_get_file.sh tcp "/home/ubuntu/raw-packets/legit/legit_log.txt" results/${EXP_NAME}
+./ec2_send_command.sh tcp "rm -rf /home/ubuntu/raw-packets/legit/legit_log.txt"
+./ec2_get_file.sh tcp "/home/ubuntu/legit_dump.pcap" results/${EXP_NAME}
+./ec2_send_command.sh tcp "rm -rf /home/ubuntu/legit_dump.pcap"
 
+# Once the instance has had enough time, collect the server log.
 ./ec2_get_file.sh dest "/home/ubuntu/server_ifconfig.txt" results/${EXP_NAME}
-./ec2_get_file.sh client "/home/ubuntu/client_ifconfig.txt" results/${EXP_NAME}
-./ec2_get_file.sh tcp "/home/ubuntu/raw-packets/legit/output.txt" results/${EXP_NAME}
-
 ./ec2_send_command.sh dest "rm -rf /home/ubuntu/server_ifconfig.txt"
-./ec2_send_command.sh client "rm -rf /home/ubuntu/client_ifconfig.txt"
-./ec2_send_command.sh tcp "rm -rf /home/ubuntu/raw-packets/legit/output.txt"
